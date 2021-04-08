7f0a782a3e297327b2e29920ec73bb8e93b48edc
James O'Brien <james@blitzm.com>
2021-04-08 15:23:36 +1000

    Update module settings with new config

---
+++ nrtpredict/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from .cli import *
+
+if __name__ == "__main__":
+    cli.cli_entry()
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='nrt-predict',
+    name='nrtpredict',
     version='0.1',
     description='Near Real Time Library',
     url='https://github.com/daleroberts/nrt-predict',
     author='Dale Roberts',
     author_email='dale.roberts@anu.edu.au',
     license='To Be Determined',
-    packages=['models'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'nrtpredict = nrtpredict.cli:cli_entry',
+        ],
+    },
     install_requires=[
           'requests',
           'pyyaml',
@@ -20,11 +25,8 @@ setup(
           'gdal',
           'joblib'
       ],
-    scripts=[
-        'nrt_predict.py'
-    ],
     package_data={
-
+        'nrtpredict': [ 'nrtpredict/config/nrt-default-config.yaml' ]
     },
     include_package_data=True,
     zip_safe=False
